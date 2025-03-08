207f3626b41fd1b420d419854c00849ce8813bad
jd <jhojan_54_56@hotmail.com>
2025-03-07 23:53:58 -0500

    test

---
+++ src/test/java/com/mensajeriaservice/microserviceMensajeria/MicroserviceMensajeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mensajeriaservice.microserviceMensajeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceMensajeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
